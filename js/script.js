42b206cb6369ad42807e872200283f16666dcfff
dru_o <dru404@yahoo.com>
2021-07-20 10:48:24 -0400

    Updated shop page

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
+
 const today = new Date();
 const year = today.getFullYear();
+
 
 const copyRight= document.querySelector(".copyright");
 copyRight.innerHTML = `<p>Copyright ${year} | dru oliver</p>`;
